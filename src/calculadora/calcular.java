18961641a528596e361b7f6509e96c062cb95d47
Douglas Oliveira <douglas.tom@hotmail.com>
2016-04-25 13:54:30 -0300

    Added files via upload

---
+++ src/calculadora/calcular.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Douglas
+ */
+public class calcular {
+    
+    
+    
+    
+    public double somar(double a,double b){
+    
+    return a+b;
+
+    }
+    public double subtrair(double a,double b){
+    
+    return a-b;
+
+    }
+    public double dividir(double a,double b){
+    
+    return a/b;
+
+    }
+    public double multiplicar(double a,double b){
+    
+    return a*b;
+
+    }
+    
+    
+    
+    
+    
+    
+    
+}
